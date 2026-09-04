105be865ca9763a8e4e61fb60a0940eb061da375
Tifas2026 <jonas.raffeiner@father-son.it>
2026-09-04 10:19:53 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+/* Offline-Cache. Bei Änderungen an den Dateien die Version hochzählen. */
+const CACHE = "abnahme-v1";
+const FILES = ["./", "./index.html", "./jspdf.umd.min.js", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
